67c213d3988ea670c60f091a4135a2424580193d
jseimKSU <jseim@ksu.edu>
2025-09-06 20:03:44 -0500

    fixed hitbox of stick guy

--- Sprites/StickFigureSprite.cs
+++ Sprites/StickFigureSprite.cs
@@ -6,6 +6,7 @@ using Parkour2D360.Collisions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,51 +14,81 @@ namespace Parkour2D360.Sprites
 {
     public class StickFigureSprite
     {
-        private const int MAX_RUN_INT_VALUE = 6;
-        private const int MAX_IDLE_INT_VALUE = 7;
-        private const string SPRITE_FILE_RELATIVE_PATH = "SpriteTextures/StickFigureCharacterSprites2D/Fighter sprites/";
+        private const int RUNNING_ANIMATION_FRAMES_NUMBER = 8;
+        private const int IDLE_ANIMATION_FRAMES_NUMBER = 8;
+        private const string SPRITE_FILES_RELATIVE_PATH = "SpriteTextures/StickFigureCharacterSprites2D/Fighter sprites/CombinedSprites";
         private const int PLAYER_SPEED = 6;
         private const double ANIMATION_SPEED = .1;
 
         private GamePadState _gamePadState;
         private KeyboardState _keyboardState;
 
-        private Texture2D[] _runningTextures = new Texture2D[MAX_RUN_INT_VALUE+1];
+        private Texture2D _runningTextures;
         private int _previousRunningState;
         private int _currentRunningState;
+        private Rectangle[] _runningAnimationFrameSourceRectangles;
 
-        private Texture2D[] _idleTextures = new Texture2D[MAX_IDLE_INT_VALUE+1];
+        private Texture2D _idleTextures;
         private int _previousIdleState;
         private int _currentIdleState;
+        private Rectangle[] _idleAnimationFrameSourceRectangles;
+
+        private double _animationTimer;
+
         private bool _isMoving;
         private bool _isColliding;
 
         private Vector2 _position;
         private bool _flipped;
         private BoundingRectangle _hitbox;
-        private double _animationTimer;
         public BoundingRectangle Hitbox => _hitbox;
 
         public void Initalize()
         {
             _currentRunningState = 0;
-            _currentIdleState = 0;
-            _position = new Vector2(-130, 651);
+            _currentIdleState = 0;w
+            _position = new Vector2(0, 811);
             _hitbox.ChangePositionTo(_position);
-            _hitbox.ChangeDimentions((float)(512 * .75), (float)(512 * .75));
+
         }
 
         public void LoadContent(ContentManager content)
         {
-            // have to either combine all the needed sprites to one file or Load based on some calculation with gameTime
-            for (int i = 0; i <= MAX_RUN_INT_VALUE; i++)
-            {
-                _runningTextures[i] = content.Load<Texture2D>($"{SPRITE_FILE_RELATIVE_PATH}Run/run{i + 1}");
-            }
-            for (int i = 0;i <= MAX_IDLE_INT_VALUE; i++)
-            {
-                _idleTextures[i] = content.Load<Texture2D>($"{SPRITE_FILE_RELATIVE_PATH}Idle/idle{i}");
-            }
+            _idleTextures = content.Load<Texture2D>($"{SPRITE_FILES_RELATIVE_PATH}/idle");
+            _runningTextures = content.Load<Texture2D>($"{SPRITE_FILES_RELATIVE_PATH}/run");
+
+            FillIdleAnimationFrameSourceRectangles();
+            FillRunningAnimationFrameSourceRectangles();
+
+            _hitbox.ChangeDimentions(_idleAnimationFrameSourceRectangles[0].Width, _idleAnimationFrameSourceRectangles[0].Height);
+        }
+
+        private void FillIdleAnimationFrameSourceRectangles()
+        {
+            _idleAnimationFrameSourceRectangles = new Rectangle[IDLE_ANIMATION_FRAMES_NUMBER];
+
+            _idleAnimationFrameSourceRectangles[0] = new Rectangle(0,0, 109, 167);
+            _idleAnimationFrameSourceRectangles[1] = new Rectangle(110, 0, 110, 167);
+            _idleAnimationFrameSourceRectangles[2] = new Rectangle(0, 168, 109, 172);
+            _idleAnimationFrameSourceRectangles[3] = new Rectangle(110, 168, 110, 172);
+            _idleAnimationFrameSourceRectangles[4] = new Rectangle(0, 340, 108, 167);
+            _idleAnimationFrameSourceRectangles[5] = new Rectangle(109, 340, 111, 167);
+            _idleAnimationFrameSourceRectangles[6] = new Rectangle(0, 507, 109, 173);
+            _idleAnimationFrameSourceRectangles[7] = new Rectangle(110, 507, 110, 173);
+        }
+
+        private void FillRunningAnimationFrameSourceRectangles()
+        {
+            _runningAnimationFrameSourceRectangles = new Rectangle[RUNNING_ANIMATION_FRAMES_NUMBER];
+
+            _runningAnimationFrameSourceRectangles[0] = new Rectangle(0, 0, 109, 166);
+            _runningAnimationFrameSourceRectangles[1] = new Rectangle(110, 0, 124, 166);
+            _runningAnimationFrameSourceRectangles[2] = new Rectangle(234, 0, 111, 166);
+            _runningAnimationFrameSourceRectangles[3] = new Rectangle(345, 0, 111, 166);
+            _runningAnimationFrameSourceRectangles[4] = new Rectangle(0, 167, 111, 173);
+            _runningAnimationFrameSourceRectangles[5] = new Rectangle(112, 167, 109, 173);
+            _runningAnimationFrameSourceRectangles[6] = new Rectangle(221, 167, 109, 173);
+            _runningAnimationFrameSourceRectangles[7] = new Rectangle(330, 167, 109, 173);
         }
 
         public void Update(GameTime gameTime, List<BoundingRectangle> _hitboxes)
@@ -127,21 +158,25 @@ namespace Parkour2D360.Sprites
             {
                 if (_animationTimer > ANIMATION_SPEED)
                 {
+                    Rectangle runningState = _runningAnimationFrameSourceRectangles[_currentRunningState];
                     _previousRunningState = _currentRunningState;
-                    _currentRunningState = (_previousRunningState < MAX_RUN_INT_VALUE) ? _currentRunningState + 1 : 0;
+                    _currentRunningState = (_previousRunningState < RUNNING_ANIMATION_FRAMES_NUMBER-1) ? _currentRunningState + 1 : 0;
+                    _hitbox.ChangeDimentions((float)(runningState.Width * 0.75), (float)(runningState.Height * 0.75));
                     _animationTimer -= ANIMATION_SPEED;
                 }
-                spriteBatch.Draw(_runningTextures[_currentRunningState], _position, null, Color.Black, 0, new Vector2(0, 0), .75f, spriteEffects, 0);
+                spriteBatch.Draw(_runningTextures, _position, _runningAnimationFrameSourceRectangles[_currentRunningState], Color.Black, 0, new Vector2(0, 0), .75f, spriteEffects, 0);
             }
             else
             {
                 if (_animationTimer > ANIMATION_SPEED)
                 {
+                    Rectangle idleState = _idleAnimationFrameSourceRectangles[_currentIdleState];
                     _previousIdleState = _currentIdleState;
-                    _currentIdleState = (_previousIdleState < MAX_IDLE_INT_VALUE) ? _currentIdleState + 1 : 0;
+                    _currentIdleState = (_previousIdleState < IDLE_ANIMATION_FRAMES_NUMBER-1) ? _currentIdleState + 1 : 0;
+                    _hitbox.ChangeDimentions((float)(idleState.Width * 0.75), (float)(idleState.Height * 0.75));
                     _animationTimer -= ANIMATION_SPEED;
                 }
-                spriteBatch.Draw(_idleTextures[_currentIdleState], _position, null, Color.Black, 0, new Vector2(0, 0), .75f, spriteEffects, 0);
+                spriteBatch.Draw(_idleTextures, _position, _idleAnimationFrameSourceRectangles[_currentIdleState], Color.Black, 0, new Vector2(0, 0), .75f, spriteEffects, 0);
             }
         }
 
